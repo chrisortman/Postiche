8183db2d82e371277061cd71ef28e944ecd64ff0
Chris Ortman <chrisortman@gmail.com>
2012-03-08 23:28:07 -0800

    Xunit attribute for shim context

--- src/XunitTestProject/AShimUsage.cs
+++ src/XunitTestProject/AShimUsage.cs
@@ -38,5 +38,36 @@ namespace XunitTestProject
 			orderService.ThisMethodIsOnlyCalledFromXunitSoIcanSeeCodeCoverage();
 			Assert.True(true);
 		}
+
+		[Fact,UseShimContext]
+		public void XunitExtensibilityIsNiceForShimContext() {
+			ShimProduct.FindByIdInt32 = id => {
+				return new Product() {
+					Id = id,
+					Name = "Product " + id,
+					Price = 5M
+				};
+			};
+
+			var orderService = new OrderService();
+			var p1 = orderService.FindProduct(1);
+			var p2 = orderService.FindProduct(2);
+
+			Assert.Equal(1, p1.Id);
+			Assert.Equal(2, p2.Id);
+		}
     }
+
+	[AttributeUsage(AttributeTargets.Method,AllowMultiple = false,Inherited = true)]
+	public class UseShimContext : BeforeAfterTestAttribute {
+		private IDisposable _context;
+
+		public override void Before(System.Reflection.MethodInfo methodUnderTest) {
+			_context = ShimsContext.Create();
+		}
+
+		public override void After(System.Reflection.MethodInfo methodUnderTest) {
+			_context.Dispose();
+		}
+	}
 }
